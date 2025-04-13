dadae1a3b12c9645d8c402a1d146c7962869e98a
Justin8900 <119088721+Justin8900@users.noreply.github.com>
2025-04-13 18:13:22 +0800

    Update models.py

--- unihaven/models.py
+++ unihaven/models.py
@@ -345,8 +345,8 @@ class Reservation(models.Model):
     start_date = models.DateField()
     end_date = models.DateField()
     cancelled_by = models.CharField(max_length=50, null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
 
     
     def __str__(self):
