bdafe1c7bd27055c9cb8c9ac09f599233f9b6067
Justin8900 <119088721+Justin8900@users.noreply.github.com>
2025-04-13 17:51:23 +0800

    Update 0006_reservation_created_at_reservation_updated_at.py

--- unihaven/migrations/0006_reservation_created_at_reservation_updated_at.py
+++ unihaven/migrations/0006_reservation_created_at_reservation_updated_at.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name="reservation",
             name="created_at",
-            field=models.DateTimeField(auto_now_add=True, default=now),
+            field=models.DateTimeField(auto_now_add=True, default=django.utils.timezone.now),
             preserve_default=False,
         ),
         migrations.AddField(
