8728fbbd816b558e25ba31c1ef57f00aa1a61c5c
Justin8900 <119088721+Justin8900@users.noreply.github.com>
2025-04-13 16:55:04 +0800

    Update apps.py

--- unihaven/apps.py
+++ unihaven/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class UnihavenConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'unihaven'
+    def ready(self):
+        import unihaven.models
